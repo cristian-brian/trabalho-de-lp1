2cebd1a45df8289f4f53e6abb651b3187cd39900
cristian-brian <cristiann.briann@gmail.com>
2025-07-11 20:39:10 -0300

    Update main3.c

--- main3.c
+++ main3.c
@@ -28,7 +28,6 @@ ponto maiorVigilante ( ponto * pontos, int numPontos, aresta *arestas, int numAr
             maior = contador;
             maiorPonto = pontos[i];
         }
-
     }
     return maiorPonto;
 }
@@ -37,7 +36,7 @@ int temMaiorVigilante (ponto p1, aresta a1){
     return ( p1.coordenada == a1.p1.coordenada || p1.coordenada == a1.p2.coordenada);
 }
 
-int quantasMaiorVigilante(ponto p1, aresta *arestas, int numArestas){
+int quantasMaiorVigilanteOlhando(ponto p1, aresta *arestas, int numArestas){
     int contador = 0;
     for ( int i = 0; i < numArestas; i++){
         contador += temMaiorVigilante(p1, arestas[i]);
@@ -45,18 +44,30 @@ int quantasMaiorVigilante(ponto p1, aresta *arestas, int numArestas){
     return contador;
 }
 
-
-/*
-
-void eliminarRuasVigiadas(ponto p1, aresta *arestas, int numArestas){
-    for ( int i = 0; i < numArestas; i++ ){
-        for ( int j = i + 1; j < numArestas - 1; j++){
-            if ( p1.coordenada == arestas[])
+void eliminarRuasVigiadas(ponto p1, aresta *arestas, int *numArestas){
+    int arestasFree = quantasMaiorVigilanteOlhando(p1, arestas, *numArestas), j = 0;
+    for ( int i = 0; i < *numArestas; i++ ){
+        if ( !temMaiorVigilante( p1, arestas[i])){
+            arestas[j++] = arestas [i];            
         }
     }
+    *numArestas = j;
 }
 
-*/
+void analisarCameras (ponto *pontos, int *numPontos, aresta *arestas, int numArestas){
+    int k = 0;
+    for ( int i = 0; i < *numPontos; i++ ){
+        for ( int j = 0; j < numArestas; j++){
+            if ( pontos[i].coordenada == arestas[j].p1.coordenada || pontos[i].coordenada == arestas[j].p2.coordenada){
+                pontos[k++] = pontos[i];
+                break;
+            }
+        }
+    }
+    *numPontos = k;
+
+}
+
 int main (void){
 
     // temos 5 pontos
@@ -65,18 +76,41 @@ int main (void){
 
     aresta *ruasVigiadas = (aresta*) malloc ( 6 * sizeof(aresta));
 
-    for ( int i = 0; i < 5; i++){
+    int numRuas = 6, numCamera = 5;
+
+    for ( int i = 0; i < numCamera; i++){
         cameras[i].coordenada = i;
     }
 
-    for ( int i = 0; i < 6; i++){
+    for ( int i = 0; i < numRuas; i++){
         printf ("Digite as coordenadas das cameras que vigiam a mesma rua --> ");
         scanf ("%d %d", &ruasVigiadas[i].p1.coordenada, &ruasVigiadas[i].p2.coordenada);
     }
-    ponto p4 = maiorVigilante(cameras, 5, ruasVigiadas, 6);
 
-    printf ("%d\n", p4.coordenada);
+    ponto *resposta = (ponto *) malloc (3 * sizeof(ponto)); // botei 3 pqq ja sei que a resposta é 3, dps ver como ajeita
 
+    int i = 0;
+
+    while ( numRuas != 0 ){
+        ponto a = maiorVigilante( cameras, numCamera, ruasVigiadas, numRuas);
+
+        resposta[i++] = a;
+
+        eliminarRuasVigiadas(a, ruasVigiadas, &numRuas);
+
+        analisarCameras(cameras, &numCamera, ruasVigiadas, numRuas);
+
+    }
+
+    printf("Cameras com maior vigilancia em ordem:\n");
+
+    for (int j = 0; j < i; j++) {
+        printf("Camera coordenada: %d\n", resposta[j].coordenada);
+    }
+
+    free(cameras);
+    free(ruasVigiadas);
+    free(resposta);
 
     return 0;
 }
