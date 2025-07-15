a420f88341a89078677942173621659ad1facdee
cristian-brian <cristiann.briann@gmail.com>
2025-07-14 23:18:47 -0300

    Update main3.c

--- main3.c
+++ main3.c
@@ -14,10 +14,10 @@ struct Aresta {
 
 typedef struct Aresta aresta;
 
-ponto maiorVigilante ( ponto * pontos, int numPontos, aresta *arestas, int numArestas){
+ponto maiorVigilante ( ponto * pontos, int numPontos, aresta *arestas, int numArestas){ 
     int contador, maior = 0;
     ponto maiorPonto;
-    for ( int i = 0; i < numPontos; i++){
+    for ( int i = 0; i < numPontos; i++){ 
         contador = 0;        
         for ( int j = 0; j < numArestas; j++){
             if ( pontos[i].coordenada == arestas[j].p1.coordenada || pontos[i].coordenada == arestas[j].p2.coordenada){
@@ -44,7 +44,7 @@ int quantasMaiorVigilanteOlhando(ponto p1, aresta *arestas, int numArestas){
     return contador;
 }
 
-void eliminarRuasVigiadas(ponto p1, aresta *arestas, int *numArestas){
+void eliminarRuasVigiadas(ponto p1, aresta *arestas, int *numArestas){ 
     int j = 0;
     for ( int i = 0; i < *numArestas; i++ ){
         if ( !temMaiorVigilante( p1, arestas[i])){
@@ -69,33 +69,44 @@ void analisarCameras (ponto *pontos, int *numPontos, aresta *arestas, int numAre
 
 void addCamera(ponto **cameras, ponto a, int *tamAtual) {
     (*tamAtual)++;
-    ponto *temp = realloc(*cameras, (*tamAtual) * sizeof(ponto));
+    ponto *temp = realloc(*cameras, (*tamAtual) * sizeof(ponto)); 
 
     if (temp == NULL) {
-        printf("Erro ao realocar memória\n");
+        printf("Erro ao realocar memoria\n");
     }
 
     *cameras = temp;
     (*cameras)[(*tamAtual) - 1] = a; 
 }
 
+void exibirResultados (FILE *ptr, ponto *pontos, int tamPontos){
+
+    int static i = 0;   
+
+    if ( i == tamPontos){
+        i = 0;
+        fprintf (ptr, "Foram necesarias %d cameras\n\n", tamPontos);
+    } else {
+        fprintf(ptr, "Camera coordenada: %d\n", pontos[i].coordenada);
+        i++;
+        exibirResultados (ptr, pontos, tamPontos);
+    }
+}
+
 
 int main (void){
 
-     int numRuas, numCamera;
-    
-    
+    int numRuas, numCamera;
     FILE *ptr;
 
-    if ( (ptr = fopen("n_8_m_12.txt", "r")) == NULL){
+    if ( (ptr = fopen("n_10_m_15.txt", "r")) == NULL){ 
         printf ("Arquivo não pode ser aberto");
         return 0;
     } else {
-        fscanf(ptr, "%d %d", &numCamera, &numRuas);
+        fscanf(ptr, "%d %d", &numCamera, &numRuas); 
     }
 
     ponto *cameras = (ponto *) malloc ( numCamera * sizeof (ponto));
-
     aresta *ruasVigiadas = (aresta*) malloc ( numRuas * sizeof(aresta));
 
     for ( int i = 0; i < numCamera; i++){
@@ -109,8 +120,7 @@ int main (void){
     fclose(ptr);
 
     ponto *resposta = NULL; 
-    
-    int totalCameras = 0;
+    int totalCameras = 0;   
 
     while ( numRuas != 0 ){
         ponto a = maiorVigilante( cameras, numCamera, ruasVigiadas, numRuas);
@@ -123,11 +133,18 @@ int main (void){
 
     }
 
-    printf("Cameras com maior vigilancia em ordem:\n");
+    FILE *ptr2;
 
-    for (int j = 0; j < totalCameras; j++) {
-        printf("Camera coordenada: %d\n", resposta[j].coordenada);
+    ptr2 = fopen("saida.txt", "a+");
+
+    if (ptr2 == NULL) {
+    printf("Erro ao abrir arquivo de saída\n");
+    return 1;
     }
+
+    exibirResultados(ptr2, resposta, totalCameras);
+
+    fclose(ptr2);
 
     free(cameras);
     free(ruasVigiadas);
